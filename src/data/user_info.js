0d76dec959264b06e7bce2469706e97cd2a95681
tasinvoid <shoaibmahmud4000@gmail.com>
2025-08-12 14:22:22 +0600

    json ready

---
+++ src/data/user_info.js
@@ -0,0 +1,74 @@
+const info = {
+  // ============ MAIN DETAILS ============
+  main: {
+    name: "Shoaib Mahmud Tasin",
+    description:
+      "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using MERN stack. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+    role: "Full-Stack Developer",
+    photo:
+      "https://i.ibb.co.com/xngp0Y4/8a-NISOx-PAhb-Zoufm-BUt-Ml-ZCoc-KJ.jpg",
+    email: "shoaibmahmudtasin@gmail.com",
+  },
+
+  // ============ SOCIAL LINKS ============
+  socials: {
+    github: "https://github.com/tasinvoid",
+    linkedin: "www.linkedin.com/in/shoaib-mahmud-tasin",
+    facebook: "https://www.facebook.com/profile.php?id=61573135861799",
+  },
+
+  // ============ PROJECTS ============
+  projects: [
+    {
+      title: "Buildia: Smart Building Management",
+      description:
+        "A comprehensive web application for managing a single building, with role-based dashboards for users, members, and admins. It streamlines apartment rentals, payment processing, and community announcements.",
+      technologies:
+        "React, Node.js, Express, MongoDB, Firebase, JWT, TanStack Query",
+      github: "https://github.com/tasinvoid/buildia-client",
+      link: "https://buildia-92b37.web.app/",
+    },
+    {
+      title: "Sproutly: A Gardening Community Hub",
+      description:
+        "A vibrant community platform for gardening enthusiasts. It enables users to share tips, find local gardeners, and engage in discussions on various topics from plant care to composting.",
+      technologies: "React, Node.js, Express, MongoDB, Firebase",
+      github: "https://github.com/tasinvoid/A10-Sproutly-client",
+      link: "https://sproutly-515fc.web.app/",
+    },
+    {
+      title: "Food-circle: A Food Sharing Platform",
+      description:
+        "A full-stack web application designed to combat food waste by connecting users with surplus food. It features comprehensive CRUD operations, a food request system, and secure authentication.",
+      technologies:
+        "React, Node.js, Express, MongoDB, Firebase, JWT, Framer Motion, TanStack Query",
+      github: "https://github.com/tasinvoid/food-circle-client",
+      link: "https://food-circle-fce91.web.app/",
+    },
+  ],
+  // ============ EDUCATION ============
+  education: [
+    {
+      school: "Yunnan University",
+      degree: "Bachelor of Electrical Information Engineering",
+      duration: "2018 - 2022",
+    },
+    {
+      school: "Programming Hero",
+      degree: "Web Development",
+      duration: "2025 - 2025",
+    },
+  ],
+
+  // ============ CONTACT ============
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at shoaibmahmudtasin@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+  },
+
+  // ============ FOOTER ============
+  footer: "© 2024 Daniel Shan Balico. All Rights Reserved",
+};
+
+export default info;
